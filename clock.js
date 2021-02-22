8999890c8c207b56735c6233785bfb68710fb072
Naveen Bandarage <thebanterage@gmail.com>
2021-02-22 20:10:16 +1300

    first commit

--- clock.js
+++ clock.js
@@ -1,9 +1,33 @@
-window.onload = function () {
-  setInterval(count, 1000);
-};
+function Counter(elem, delay) {
+  var value = parseInt(elem.innerHTML, 10);
+  var interval;
 
-let counter = 0;
-function count() {
-  counter++;
-  //do stuff
+  function increment() {
+    return (value += 1);
+  }
+
+  function updateDisplay(value) {
+    elem.innerHTML = value;
+  }
+
+  function run() {
+    updateDisplay(increment());
+  }
+
+  function start() {
+    interval = window.setInterval(run, delay);
+  }
+
+  // exports
+  // This actually creates a function that our counter can call
+  // you'll see it used below.
+  //
+  // The other functions above cannot be accessed from outside
+  // this function.
+  this.start = start;
 }
+
+var elem = document.getElementById("txt");
+
+counter = new Counter(elem, 500);
+counter.start();
